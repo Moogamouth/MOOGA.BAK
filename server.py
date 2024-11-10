00f6d38547d146f423d0a9bac66a181c9b2d4798
Isaac Weingrad <48636131+Moogamouth@users.noreply.github.com>
2024-11-10 16:40:13 -0500

    Added broken zpaq

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 from internetarchive import search_items, download
 from tempfile import gettempdir
 from requests.exceptions import HTTPError
+import subprocess
+import os
 
 search = search_items("*")
 for result in search:
     id = result["identifier"]
-    path = gettempdir() + "\\" + id
+    temp = gettempdir()
+    dir_path = os.path.join(temp, "archive")
+    zpaq = dir_path + ".zpaq"
     try:
-        download(id, destdir=gettempdir())
+        download(id, destdir=dir_path)
+        command = "./zpaq.exe a " + zpaq + " " + dir_path
+        subprocess.run(command, shell=True)
     except HTTPError as e:
         if e.response.status_code == 403:
             pass
