be7d8be91909f34583d0f2bd99ec8a9f63feba9d
Isaac Weingrad <48636131+Moogamouth@users.noreply.github.com>
2024-11-10 17:42:50 -0500

    added compression

--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 from internetarchive import search_items, download
-from tempfile import gettempdir
 from requests.exceptions import HTTPError
-import subprocess
 import os
+import subprocess
+
+cwd = os.getcwd()
+downloads = os.path.join(cwd, "downloads")
+compressed = os.path.join(cwd, "archive.zpaq")
 
 search = search_items("*")
 for result in search:
+    break
     id = result["identifier"]
-    temp = gettempdir()
-    dir_path = os.path.join(temp, "archive")
-    zpaq = dir_path + ".zpaq"
     try:
-        download(id, destdir=dir_path)
-        command = "./zpaq.exe a " + zpaq + " " + dir_path
-        subprocess.run(command, shell=True)
+        download(id, destdir=downloads)
     except HTTPError as e:
         if e.response.status_code == 403:
             pass
+
+#command = ["zpaq", "add", compressed, downloads, "-m2"]
+#subprocess.run(command, check=True)
+
+megabyte = 1024 * 1024
+with open(compressed, 'rb') as file:
+    while True:
+        chunk = file.read(megabyte)
+        if not chunk:
+            break
+        print(f"Processing chunk of size {len(chunk)} bytes")
